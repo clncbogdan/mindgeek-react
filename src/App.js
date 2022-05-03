2d7d7f7e9b6aaeaedb453cb92ab21ecce7004e2d
Bogdan Calancea <bogdan.c@mondly.com>
2022-05-03 12:52:44 +0300

    Add routes and import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
-function App() {
+import "bootstrap/dist/css/bootstrap.css";
+
+import MovieListPage from './pages/movie-list-page/movie-list-page.component';
+import MovieDetailsPage from './pages/movie-details-page/movie-details-page.component';
+
+
+export default function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router basename="/">
+      <Routes>
+        <Route path="/" element={<MovieListPage />} />
+        <Route path={'/movies/:id'} element={<MovieDetailsPage />} />
+      </Routes>
+    </Router>
+  )
 }
-
-export default App;
